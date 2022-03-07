b4a133ecde2ac0b6b81c1acfbe8b4acd5d0e9cea
zhailiqiong <zhailiqiong@123.com>
2022-03-07 17:03:03 +0800

    feat: 3,5,7倍数，和包含3, 5, 7的数

--- src/fizzBuzzConverter.ts
+++ src/fizzBuzzConverter.ts
@@ -1,8 +1,23 @@
-export type FizzBuzzType = number | string;
+type FizzBuzzType = number | string;
 
-
-const fizzBuzzConverter = (number: number): FizzBuzzType => {
-    return 0;
+let numbers: {[key: string]: any} = {
+    '3': 'Fizz',
+    '5': 'Buzz',
+    '7': 'Whizz'
 }
 
-export default fizzBuzzConverter;
+const fizzBuzzConverter = (number: number): FizzBuzzType => {
+    let res = ''
+    let keys = Object.keys(numbers)
+    if (number > 2) {
+        keys.forEach((i) => {
+            res += number % +i === 0 || JSON.stringify(number).indexOf(i) !== -1 ? numbers[i] : ''
+        })
+    } else {
+        res = JSON.stringify(number)
+    }
+    return res
+}
+
+
+console.log(fizzBuzzConverter(30));
