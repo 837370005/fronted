16519cfca3d8bee6fd6cffad9fdd21d8afdceb1f
zhailiqiong <zhailiqiong@123.com>
2022-03-07 18:33:29 +0800

    feat: 包含了3,5,7时，就忽略2，3条件和特殊数的倍数下

--- src/fizzBuzzConverter.ts
+++ src/fizzBuzzConverter.ts
@@ -3,16 +3,22 @@ type FizzBuzzType = number | string;
 let numbers: {[key: string]: any} = {
     '3': 'Fizz',
     '5': 'Buzz',
-    '7': 'Whizz'
+    '7': 'Whizz',
 }
 
 const fizzBuzzConverter = (number: number): FizzBuzzType => {
     let res = ''
     let keys = Object.keys(numbers)
     if (number > 2) {
-        keys.forEach((i) => {
-            res += number % +i === 0 || JSON.stringify(number).indexOf(i) !== -1 ? numbers[i] : ''
-        })
+        for (let i of keys) {
+            if (JSON.stringify(number).indexOf(i) !== -1) {
+                res = numbers[i]
+                return res
+            } else if (number % +i === 0) {
+                console.log(2)
+                res += numbers[i]
+            }
+        }
     } else {
         res = JSON.stringify(number)
     }
